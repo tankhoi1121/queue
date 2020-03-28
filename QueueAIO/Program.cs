590ee88868e8fdc0224d653e331feb4c6ebe452a
khoi.nt <khoi.nt@outlook.com>
2020-03-28 20:48:51 +0700

    Queue commit

---
+++ QueueAIO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace QueueAIO
+{
+    class Program
+    {
+        static void PrintQueue(int[] queue, int max)
+        {
+            for (var i = 0; i < max; i++)
+            {
+                Console.WriteLine($"{queue[i]}; ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] queue = new int[10];
+            int max = queue.Length;
+
+            int rear = 0;
+            int front = 0;
+
+            QueueBehavior queueBehavior = new QueueBehavior();
+
+            queueBehavior.enqueue(queue, max, ref rear, ref front, 44);
+            queueBehavior.enqueue(queue, max, ref rear, ref front, 21);
+            queueBehavior.enqueue(queue, max, ref rear, ref front, 26);
+            queueBehavior.enqueue(queue, max, ref rear, ref front, 33);
+            queueBehavior.enqueue(queue, max, ref rear, ref front, 13);
+            queueBehavior.enqueue(queue, max, ref rear, ref front, 4);
+
+            //PrintQueue(queue, max);
+            Console.WriteLine(queueBehavior.dequeue(queue, max, ref rear, ref front));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QueueAIO/QueueBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueAIO
+{
+    public class QueueBehavior
+    {
+        #region -- Status --
+        private bool isFull(int max, ref int rear)
+        {
+            if (rear + 1 == max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        private bool isEmpty(int max, ref int rear, ref int front)
+        {
+            if (front == -1 || front > rear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region -- Operator --
+        public void enqueue(int[] queue, int max, ref int rear, ref int front, int values)
+        {
+            if (isFull(max, ref rear))
+            {
+                Console.WriteLine("Overflow");
+            }
+            else
+            {
+
+                queue[rear] = values;
+                rear++;
+            }
+        }
+
+        public int dequeue(int[] queue, int max, ref int rear, ref int front)
+        {
+            int data = 0;
+            if (isEmpty(max, ref rear, ref front))
+            {
+                Console.WriteLine("Underflow");
+            }
+            else
+            {
+                data = queue[front];
+                front++;
+            }
+            return data;
+        }
+        #endregion
+    }
+}
